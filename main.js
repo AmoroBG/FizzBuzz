a586800f73d2542b7495f060c736f82414667a5c
Amoro Bagei <amoro.bagei@amalitech.com>
2021-07-24 17:19:40 -0700

    Add parseInt to ensure the number is an Integer

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Prompt user for the number
-let number = prompt("Enter a number")
+let number = parseInt(prompt("Enter a number"))
 for (var i = 1; i <= number; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    if ((i % 3 === 0) && (i % 5 == 0)) {
         // multiples of both three and five print 'FizzBuzz'
         console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
+    } else if (i % 3 === 0) {
         // multiples of three print 'Fizz'
         console.log("Fizz");
-    } else if (i % 5 == 0) {
+    } else if (i % 5 === 0) {
         // multiples of five print 'Buzz'
         console.log("Buzz");
     } else {
