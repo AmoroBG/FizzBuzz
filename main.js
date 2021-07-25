de36d3a914607810cb48156c0352529d03fa1dbd
Amoro Bagei <amoro.bagei@amalitech.com>
2021-07-24 17:16:22 -0700

    Remove (continue) keyword

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ for (var i = 1; i <= number; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
         // multiples of both three and five print 'FizzBuzz'
         console.log("FizzBuzz");
-        continue
     } else if (i % 3 == 0) {
         // multiples of three print 'Fizz'
         console.log("Fizz");
-        continue
     } else if (i % 5 == 0) {
         // multiples of five print 'Buzz'
         console.log("Buzz");
-        continue
+    } else {
+        console.log(i);
     }
-    console.log(i);
+
 }
